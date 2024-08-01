12777b5cfa1b7bfd8697e56579764e6ffe591d92
Vinayak Soni <vinayaksoni1729@gmail.com>
2024-08-01 23:41:04 +0530

    update LL

--- testcode.cpp
+++ testcode.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
 using namespace std;
 
-struct node{
+// Node structure
+struct Node {
     int data;
-    node* next;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
 };
- 
-void printList(node* head){
-    while(head){
+
+// Function to print the linked list
+void printList(Node* head) {
+    while (head) {
         cout << head->data << " ";
         head = head->next;
     }
     cout << endl;
 }
 
+// Function to append a new node at the end
+void append(Node*& head, int newData) {
+    Node* newNode = new Node(newData);
+    if (!head) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while (temp->next) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+int main() {
+    Node* head = nullptr;
+
+    append(head, 10);
+    append(head, 20);
+    append(head, 30);
+    append(head, 40);
+
+    cout << "Linked List: ";
+    printList(head);
+
+    return 0;
+}
