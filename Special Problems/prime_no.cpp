ed38011becb53185e7605e84a868b8c5735ff8e2
Vinayak Soni <vinayaksoni1729@gmail.com>
2024-08-09 22:31:42 +0530

    prime or not

---
+++ Special Problems/prime_no.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int prime(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return false;
+    }
+    else
+    {
+        int mid = n / 2;
+        for (int i = 2; i < mid; i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+int main()
+{
+    int n;
+    string ans;
+    cout << "Enter a number to check-";
+    cin >> n;
+    bool isPrime=prime(n);
+    if (!isPrime && n>1)
+    {
+        ans = "Composite";
+    }
+    else if(isPrime)
+    {
+        ans = "Prime";
+    }
+    else{
+        ans="None";
+    }
+    cout << "Entered number " << n << " is " << ans << endl;
+}
