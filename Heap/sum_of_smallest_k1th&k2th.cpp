c888728ff10d6e389cbb2ba277cc51dfbe8c3e82
Vinayak Soni <vinayaksoni1729@gmail.com>
2024-12-15 00:49:11 +0530

    minor bugs fixed

--- Heap/sum_of_smallest_k1th&k2th.cpp
+++ Heap/sum_of_smallest_k1th&k2th.cpp
@@ -2,7 +2,7 @@
 #include <queue>
 using namespace std;
 
-void function(int arr[],int n,int k,vector<int> num){
+int findKthSmallest(int arr[],int n,int k){
     priority_queue<int> maxHeap;
     for(int i=0;i<n;i++){
         maxHeap.push(arr[i]);
@@ -10,13 +10,13 @@ void function(int arr[],int n,int k,vector<int> num){
         if(maxHeap.size()>k){
             maxHeap.pop();
         } 
-        num.push_back(maxHeap.top());
     }
+    return maxHeap.top();
 }
-int sumfinder(vector<int> num,int arr[],int n){
-    int sum;
-    int firstno=num[0];
-    int secondno=num[1];
+int sumfinder(int arr[],int n,int k1,int k2){
+    int firstno=findKthSmallest(arr,n,k1);
+    int secondno=findKthSmallest(arr,n,k2);
+    int sum=0;
     for(int i = 0; i <n; i++){
         if(arr[i] > firstno && arr[i] < secondno){
             sum=sum+i;
@@ -27,10 +27,7 @@ int sumfinder(vector<int> num,int arr[],int n){
 int main(){
     int arr[]{1,3,12,5,15,11};
     int n=sizeof(arr)/sizeof(arr[0]);
-    vector<int> num={};
     int k1=3,k2=6;
-    function(arr,n,k1,num);
-    function(arr,n,k2,num);
-    int result=sumfinder(num,arr,n);
+    int result=sumfinder(arr,n,k1,k2);
     cout<<"Sum is "<<result;
 }
