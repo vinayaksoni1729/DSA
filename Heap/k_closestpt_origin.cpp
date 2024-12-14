ccb9b19dd6fe713561c9f0c27e8596a51e90a7d4
Vinayak Soni <vinayaksoni1729@gmail.com>
2024-12-14 23:37:11 +0530

    more enhanced code for k closest

--- Heap/k_closestpt_origin.cpp
+++ Heap/k_closestpt_origin.cpp
@@ -4,50 +4,27 @@
 #include <cmath>
 using namespace std;
 
-// Function to calculate the distance from the origin
-double calculateDistance(int x, int y) {
-    return sqrt(x * x + y * y);
-}
+void finder(int arr[][2],int n ,int k){
+    priority_queue<pair<int, pair<int, int>>> maxHeap;
 
-vector<vector<int>> kClosestPoints(vector<vector<int>>& points, int k) {
-    // Max-heap to store the k closest points
-    // Pair structure: {distance, point index}
-    priority_queue<pair<double, int>> maxHeap;
-
-    for (int i = 0; i < points.size(); i++) {
-        // Calculate the distance of the point from the origin
-        double distance = calculateDistance(points[i][0], points[i][1]);
-
-        // Push the distance and index into the maxHeap
-        maxHeap.push({distance, i});
-
-        // If the heap size exceeds k, remove the farthest point
-        if (maxHeap.size() > k) {
-            maxHeap.pop();
-        }
-    }
-
-    // Extract the k closest points from the heap
-    vector<vector<int>> result;
-    while (!maxHeap.empty()) {
-        result.push_back(points[maxHeap.top().second]);
+for(int i=0;i<n;i++){
+    int distance=arr[i][0]*arr[i][0]+arr[i][1]*arr[i][1];
+    maxHeap.push({distance,{arr[i][0],arr[i][1]}});
+    if(maxHeap.size()>k){
         maxHeap.pop();
-    }
-
-    return result;
+    }   
 }
-
-int main() {
-    // Example points
-    vector<vector<int>> points = {{1, 3}, {-2, 2}, {5, 8}, {0, 1}};
-    int k = 2;
-
-    vector<vector<int>> result = kClosestPoints(points, k);
-
-    cout << "The " << k << " closest points to the origin are:" << endl;
-    for (const auto& point : result) {
-        cout << "[" << point[0] << ", " << point[1] << "]" << endl;
-    }
+while(!maxHeap.empty()){
+    pair<int,int> points=maxHeap.top().second;
+    cout<<points.first<<","<<points.second<<endl;
+    maxHeap.pop();
+}
+}
+int main(){
+    int arr[][2] = {{1, 3}, {-2, 2}, {5, 8}, {0, 1}};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int k=2;
+    finder(arr,n,k);
 
     return 0;
 }
--- testcode.cpp
+++ testcode.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
 using namespace std;
 
-struct Node {
-    int data;
-    Node* next;
-};
+void finder(int arr[][2],int n ,int k){
+    priority_queue<pair<int, pair<int, int>>> maxHeap;
 
-void printList(Node* n) {
-    while (n != NULL) {
-        cout << n->data << " ";
-        n = n->next;
-    }
-    cout << endl;
+for(int i=0;i<n;i++){
+    int distance=arr[i][0]*arr[i][0]+arr[i][1]*arr[i][1];
+    maxHeap.push({distance,{arr[i][0],arr[i][1]}});
+    if(maxHeap.size()>k){
+        maxHeap.pop();
+    }   
 }
- 
-int main() {
-    Node nodes[3];  // Array of 3 nodes
-
-    // Assign data and set up links
-    nodes[0].data = 1;
-    nodes[0].next = &nodes[1];
-    nodes[1].data = 2;
-    nodes[1].next = &nodes[2];
-    nodes[2].data = 3;
-    nodes[2].next = NULL;
-
-    printList(&nodes[0]);
+while(!maxHeap.empty()){
+    pair<int,int> points=maxHeap.top().second;
+    cout<<points.first<<","<<points.second<<endl;
+    maxHeap.pop();
+}
+}
+int main(){
+    int arr[][2] = {{1, 3}, {-2, 2}, {5, 8}, {0, 1}};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int k=2;
+    finder(arr,n,k);
 
     return 0;
 }
